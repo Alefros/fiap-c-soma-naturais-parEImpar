cc445a15639220cd1605633e44ccb1c764c65523
Alefros <alef.r.oliveira@gmail.com>
2021-05-22 17:48:21 -0300

    Adição de Arquivos de Código Fonte de Acordo com Nova Estrutura

---
+++ src/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PROGRAM_TITLE " *** SOMA DE NUMEROS NATURAIS ***"
+
+int somaNumerosPares(int iInicial, int iFinal);
+int somaNumerosImpares(int iInicial, int iFinal);
+int ehPar(int iNum);
+
+int main(int argc, char *argv[]) {
+
+    int iTotalPar = 0, iTotalImpar = 0, iInicial = 0, iFinal = 0;
+
+    //show title
+    printf(PROGRAM_TITLE);
+    printf("\n \n");
+
+    //request the range
+    printf("Digite o numero inicial: ");
+    scanf("%d", &iInicial);
+
+    printf("Digite o numero final: ");
+    scanf("%d", &iFinal);
+
+    iTotalImpar = somaNumerosImpares(iInicial, iFinal);
+    iTotalPar = somaNumerosPares(iInicial, iFinal);
+
+    printf("Total de numeros impares entre %d e %d = %d \n", iInicial, iFinal, iTotalImpar);
+    printf("Total de numeros pares entre %d e %d = %d \n", iInicial, iFinal, iTotalPar);
+
+  system("PAUSE");
+  return 0;
+}
+
+
+int somaNumerosPares(int iInicial, int iFinal) {
+
+    int iSomaPar = 0, iContador = 0;
+    for(iContador = iInicial; iContador <= iFinal; iContador ++) {
+
+        if(ehPar(iContador)) {
+            iSomaPar = iSomaPar + iContador;
+        }
+    }
+    return iSomaPar;
+}
+
+int somaNumerosImpares(int iInicial, int iFinal) {
+
+    int iSomaImpar = 0, iContador = 0;
+    for(iContador = iInicial; iContador <= iFinal; iContador ++) {
+
+        if(!ehPar(iContador)) {
+            iSomaImpar = iSomaImpar + iContador;
+        }
+    }
+    return iSomaImpar;
+}
+
+int ehPar(int iNum) {
+
+    return !(iNum % 2);
+}
